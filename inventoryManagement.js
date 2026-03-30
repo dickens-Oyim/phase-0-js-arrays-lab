53457b9411c7ee4bc4944c801784eabd3399e22d
dickens <dickens@gmail.com>
2026-03-30 19:09:44 +0300

    add

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,22 @@
 // Write your code here
+const products=["Laptop","Phone","Headphones","Monitor"]
+function logFirstProduct (){
+  console.log(products[0]);
+}
+function addProduct(product){
+  products.push(product);
+  console.log(`New products: ${products}`);
+}
+function updateProductName(position,newname){
+  let oldName = products[position];
+  products[position] = newname;
+  console.log(`updateProductName from ${oldName} to ${newname}`);
+}
 
+function removeLastProduct(){
+ let removedLastProduct = products.pop();
+  console.log(`removed Product: ${removedLastProduct}`);
+}
 
 
 // Export the necessary parts for testing
